084e279e68067d4698f671119f574b31debe3b60
TimberSoup <157195566+TimberSoup@users.noreply.github.com>
2024-12-04 15:00:15 -0500

    Initial Commit

---
+++ WeatherAppCombine/WeatherAppCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppCombineApp.swift
+//  WeatherAppCombine
+//
+//  Created by Timur on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
